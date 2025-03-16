012417ad75b3f98d7af45a7c8fa3f63fe6c32dcb
Shan Jiang <shanjiang2005@gmail.com>
2025-03-16 16:07:58 -0400

    did some work on the thing, changed some stuff and added a proper character controller

---
+++ SwordGame/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jumpButtonDown = false;
+    bool jumpJustPressed = false;
+
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = moveAction.ReadValue<Vector2>().x * runSpeed;
+        if (jumpAction.IsPressed())
+        {
+            jumpButtonDown = true;
+        }
+        if (jumpAction.WasPressedThisFrame())
+        {
+            jumpJustPressed = true;
+        } 
+    }
+    void FixedUpdate()
+    {
+        // move the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jumpButtonDown, jumpJustPressed);
+        jumpButtonDown = false;
+        jumpJustPressed = false;
+
+    }
+}
