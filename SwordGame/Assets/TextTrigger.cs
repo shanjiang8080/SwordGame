ebce70af4afecc72cb99c77c021aaba6275a7ee3
Shan Jiang <shanjiang2005@gmail.com>
2025-04-10 15:57:15 -0400

    fixed enemy sprite sizes

---
+++ SwordGame/Assets/TextTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    public string text;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
