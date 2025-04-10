14b273942677bd29ab3e6d44f8007c6a1e8e4c90
Shan Jiang <shanjiang2005@gmail.com>
2025-04-10 17:48:08 -0400

    okay i fixed a hud issue that looked fine on the outside but not the inside

--- SwordGame/Assets/CoinUI.cs
+++ SwordGame/Assets/CoinUI.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class CoinUI : MonoBehaviour
 {
     public static CoinUI instance;
+    public static bool isThere;
     private void Awake()
     {
-        if (instance == null)
+        if (!isThere)
         {
+            isThere = true;
             instance = gameObject.transform.GetChild(0).GetComponent<CoinUI>();
+            Debug.Log("Not destroying self");
             DontDestroyOnLoad(gameObject);
         }
         else
         {
+            Debug.Log("destroying self");
             Destroy(gameObject);
         }
     }
