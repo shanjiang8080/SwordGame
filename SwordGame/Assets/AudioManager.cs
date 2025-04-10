79ef76cdbec48b1f5124e0e6fd899bacb024a256
Will <williamngai88@gmail.com>
2025-04-10 15:32:35 -0400

    Audio, Assets,
    Bg

---
+++ SwordGame/Assets/AudioManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [Header("--------- Audio Source -----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------- Audio Clip -----------")]
+    public AudioClip background;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+    
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+
+}
+
+
+
