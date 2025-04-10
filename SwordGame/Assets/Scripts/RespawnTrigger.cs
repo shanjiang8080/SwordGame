7e8fef5a6703d6a12dacc17142008d2872d8764e
Shan Jiang <shanjiang2005@gmail.com>
2025-04-10 14:00:53 -0400

    added new levels, also changed checkpoint look and added an arrow sprite

--- SwordGame/Assets/Scripts/RespawnTrigger.cs
+++ SwordGame/Assets/Scripts/RespawnTrigger.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class RespawnTrigger : MonoBehaviour
 {
+    public Sprite litSprite;
+    public SpriteRenderer SpriteRenderer;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             // collision
             PlayerLifeCycle.CurrentRespawn = gameObject.transform.position;
+            SpriteRenderer.sprite = litSprite;
         }
     }
 
