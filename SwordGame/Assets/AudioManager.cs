18638a7a208b7b1847429eef562d8001a9a4a882
Shan Jiang <shanjiang2005@gmail.com>
2025-04-10 20:46:38 -0400

    ok i think this is final build or something
    woo

--- SwordGame/Assets/AudioManager.cs
+++ SwordGame/Assets/AudioManager.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
-
     [Header("--------- Audio Source -----------")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
-    [Header("--------- Audio Clip -----------")]
+    [Header("--------- Audio Clips -----------")]
     public AudioClip background;
+    public AudioClip secretEndingMusic;
 
     public static AudioManager instance;
 
@@ -18,24 +19,51 @@ public class AudioManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
-
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
             Destroy(gameObject);
+            return;
         }
-        
     }
-    
-    
+
     private void Start()
     {
-        musicSource.clip = background;
-        musicSource.Play();
+        HandleSceneMusic(SceneManager.GetActiveScene().name);
     }
 
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        HandleSceneMusic(scene.name);
+    }
 
+    private void HandleSceneMusic(string sceneName)
+    {
+        if (musicSource == null) return;
+
+
+        if (sceneName == "secretEnding")
+        {
+            if (musicSource.clip != secretEndingMusic)
+            {
+                musicSource.clip = secretEndingMusic;
+                musicSource.Play();
+            }
+        }
+        else
+        {
+
+            if (musicSource.clip != background)
+            {
+                musicSource.clip = background;
+                musicSource.Play();
+            }
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
 }
-
-
-
